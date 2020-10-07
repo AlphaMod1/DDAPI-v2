ac4c5f9a04ddfdce3c2f19e292faa51ee79cca93
IDTauras <48590288+IDTauras@users.noreply.github.com>
2020-10-07 10:11:11 +0300

    Header

---
+++ other/bank-v2/homepage/php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/homepage.css">
+    <link rel="stylesheet" href="../../../../css/font-awesome.min.css">
+    <title>Bank V2</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="./index.html"><img src="../img/logov2.png" alt="Logo"></a>
+        </div>
+        <div class="login">
+            <button class="login-btn"> <i class="fa fa-sign-in fa-1x"></i> Log In</button>
+        </div>
+    </header>
+    <div class="nav">
+        <a href="">Manage Account</a>
+        <a href="">Send Money</a>
+        <a href="">Request Money</a>
+    </div>
+</body>
+</html>
