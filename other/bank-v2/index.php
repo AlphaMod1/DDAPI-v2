7347be090ec96077660ee912190e99da6d18ca42
IDTauras <48590288+IDTauras@users.noreply.github.com>
2020-10-07 10:59:57 +0300

    A few fixes (my mistake)

---
+++ other/bank-v2/index.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('LOC')) {
+    die('Direct access not permitted');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./homepage/css/homepage.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <title>Bank V2</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="./index.html"><img src="./homepage/img/logov2.png" alt="Logo"></a>
+        </div>
+        <div class="login">
+            <button class="login-btn"><a href="./login/login.php"><i class="fa fa-sign-in fa-1x"></i> Log In</a></button>
+        </div>
+    </header>
+    <!-- <div class="nav">
+        <a href="">Manage Account</a>
+        <a href="">Send Money</a>
+        <a href="">Request Money</a>
+    </div> -->
+</body>
+</html>
