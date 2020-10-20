106f95bbe7592ba9e91a0141532c2f474c029954
IDTauras <48590288+IDTauras@users.noreply.github.com>
2020-10-20 13:04:21 +0300

    Commenting out the connection error

--- other/bank-v2/index.php
+++ other/bank-v2/index.php
@@ -9,9 +9,9 @@ $password = "Prodtheone1";
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 echo "Connected successfully";
 
 ?>
